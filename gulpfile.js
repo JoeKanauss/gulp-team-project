e7f95527f900b009216cada9923253777e937f89
JoeKanauss <33582866+JoeKanauss@users.noreply.github.com>
2018-04-25 13:39:33 -0500

    first Gulp commit made by Joe 04/25

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+//require gulp so gulp can run
+var gulp = require('gulp');
+
+//creating task that prints message to console to make sure everything's running a-ok
+gulp.task('print', function(){
+	console.log("Up and running so far!");
+});
